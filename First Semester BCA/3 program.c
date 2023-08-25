c1e5b88600d107c841437e0ae5ed13a28fbc51bf
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2023-08-25 14:53:29 +0530

    Delete First Semester BCA directory

--- First Semester BCA/3 program.c
+++
@@ -1,9 +0,0 @@
-// Write a program that covert temperature from Celsius to Fahrenheit and vice versa.
-#include <stdio.h>
-int main()
- {
-    int celsius, fahrenheit;
-    printf(" \t\t\tTemperature Conversion Calculator \n");
- }
-   
-; BB
