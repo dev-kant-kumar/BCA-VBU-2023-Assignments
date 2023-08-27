34bd7b4f6f356c1f01a46c739ec51f75fe570432
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2023-08-27 18:47:04 +0000

    write complete program

--- 10th_program.c
+++ 10th_program.c
@@ -1,1 +1,10 @@
 //Write a program that shows the use of conditional operator.
+#include<stdio.h>
+int main()
+ {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    number%2==0? printf("Entered number is even \n") : printf("Entered number is odd \n");
+    return 0;
+ } 
