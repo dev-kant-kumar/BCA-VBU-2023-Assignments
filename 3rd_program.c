d70bdd0e616bd7d3aad8e3d756ea7355b43769d8
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2023-08-28 06:59:53 +0000

    completed 2nd rogram

--- 3rd_program.c
+++ 3rd_program.c
@@ -1,7 +1,36 @@
 //Write a program that covert temperature from Celsius to Fahrenheit and vice versa.
-#include <stdio h>
+#include <stdio.h>
 int main()
-{ }
+ {
+    int select,celsius,fahrenheit;
+    float convert_to_fahrenheit, convert_to_celsius;
+    printf("Enter 1 to convert celsius to fahrenheit \n");
+    printf("Enter 2 to convert fahrenheit to celsius \n");
+    printf("Enter 1 or 2 from options: ");
+    scanf("%d",&select);
+    switch(select)
+     {
+        case 1: printf("Enter requested detail for temperature conversion \n");
+                printf("Enter celsius : ");
+                scanf("%d",&celsius);
+                convert_to_fahrenheit=(celsius*9/5)+32;
+                printf("%d celsius into fahrenheit =%f \n",celsius,convert_to_fahrenheit);
+                break;
+
+        case 2: printf("Enter requested detail for temperature conversion \n");
+                printf("Enter fahrenheit : ");
+                scanf("%d",&fahrenheit);
+                convert_to_celsius=(fahrenheit-32)*5/9;
+                printf("%d fahrenheit into celsius =%f \n",fahrenheit,convert_to_celsius);
+                break;
+
+        default: printf("You have entered invalid input! \n");
+                
+     }
+   return 0;
+ }
+
+
   
 
 
