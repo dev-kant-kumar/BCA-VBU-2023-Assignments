5891f288b43a5be8349a08262b4f86e679c9758e
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2023-08-29 16:28:21 +0000

    program 7th completed

--- 7th_program.c
+++ 7th_program.c
@@ -1,1 +1,33 @@
 //Write a program that prints the even and odd numbers from 1 to 100.
+#include <stdio.h>
+int main()
+ {
+    int i;
+    printf("Even numbers are : \n");
+    for(i=1;i<=100;i++)
+     {
+        if(i%2==0)
+         {
+            printf("\t %d",i);
+         }
+     }   
+    printf("\n\n");
+  
+   printf("Odd numbers are : \n");
+    for(i=1;i<=100;i++)
+     {
+        if(i%2!=0)
+         {
+            printf("\t %d",i);
+         }
+     }
+   return 0;
+ }
+
+
+    
+        
+         
+        
+     
+  
