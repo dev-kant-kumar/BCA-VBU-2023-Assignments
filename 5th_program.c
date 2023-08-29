4a067edf24c24fa408142059064f7229caaba8ee
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2023-08-29 19:50:20 +0000

    program 5th completed

--- 5th_program.c
+++ 5th_program.c
@@ -1,1 +1,21 @@
 //Write a program that finds average of a set of N numbers
+#include <stdio.h>
+int main()
+ {
+    int no_of_numbers,i,sum_of_numbers=0,input_number;
+    float average;
+  
+    printf("How many numbers do you want to find the average of : ");
+    scanf("%d",&no_of_numbers);
+    for(i=1;i<=no_of_numbers;i++)
+     {
+        printf("Enter no : ");
+        scanf("%d",&input_number);
+        sum_of_numbers+=input_number;
+        
+     }
+   average=sum_of_numbers/no_of_numbers;
+   printf("Average of entered numbers =%f \n",average);
+   return 0;
+  
+ }
