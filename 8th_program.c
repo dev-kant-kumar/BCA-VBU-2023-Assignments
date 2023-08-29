a5c42c34a7db7ca83bc777a12ed289c3775ae1f6
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2023-08-29 17:09:44 +0000

    program 8th completed

--- 8th_program.c
+++ 8th_program.c
@@ -1,1 +1,31 @@
 //Write a program that finds the greatest number between two numbers.
+#include <stdio.h>
+int main()
+ {
+    int first_number,second_number;
+    printf("Enter first no  : ");
+    scanf("%d",&first_number);
+    printf("Enter second no : ");
+    scanf("%d",&second_number);
+    if(first_number>second_number)
+     {
+        printf("%d is greatest \n",first_number);
+       
+     }
+    else if(first_number<second_number)
+     {
+        printf("%d is greatest \n",second_number);
+     }
+    else
+     {
+        printf("Both numbers are same=%d",first_number);
+     }
+   return 0;
+ }
+   
+     
+     
+     
+       
+ 
+ 
