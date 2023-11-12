b66e386f4578003a6fc781b114bd8d286c0c3a18
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-12 15:11:47 +0530

    Program 14th completed

--- 14th_program.c
+++ 14th_program.c
@@ -1,1 +1,38 @@
-// Write a program to find whether enterd number is prime or not.
+// Write a program to find whether enterd number is prime or not. 
+#include<stdio.h>
+int main()
+ {
+    int no_of_nos,i,j,num,count=0;
+    printf("Enter,how many numbers do you want to check:");
+    scanf("%d",&no_of_nos);
+    for(i=0;i<no_of_nos;i++)
+     {
+        printf("Enter number %d :",i+1);
+        scanf("%d",&num);
+        if(num>2)
+         {
+            for(j=2;j<=num/2;j++)
+             {
+                if(num%j==0)
+                 {
+                    count++;
+                    break;
+                 }    
+             }
+            if(count==0)
+             {
+                printf("Entered number is prime no \n");
+             } 
+            else
+             {
+                printf("Entered number is not a prime no \n");
+             } 
+         }
+        else
+         {
+            printf("Entered number is not a prime no \n");
+         } 
+
+     }
+
+ }
