1b42139923cf4e0c8846c61b0a7283f9224f82bc
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-12 13:40:01 +0530

    Program no 13th completed

--- 13th_program.c
+++ 13th_program.c
@@ -1,1 +1,19 @@
 // Write a program to find the number of and sum of all interger greater than 100 and less than 200 that are divisible by 7.
+#include<stdio.h>
+int main()
+ {
+    int count=0,sum=0,i;
+    printf("All intergers greater than 100 and less than 200 that are divisible by 7 are below: \n");
+    for(i=100;i<=200;i++)
+     {
+        if(i%7==0)
+         {
+            sum+=i;
+            count++;
+            printf("%d \t",i);  
+         }
+     }
+    printf("\n");
+    printf("There are %d numbers whithin this range and whose sum is %d \n",count,sum);
+     return 0; 
+ }
