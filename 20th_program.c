3abfee9f7f6072a96123fbeed30e1ef19c50e412
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-14 20:46:06 +0530

    Program no 20 completed

--- 20th_program.c
+++ 20th_program.c
@@ -1,1 +1,17 @@
 //Write a program to find reverse of a number.
+#include<stdio.h>
+int main()
+ {
+    int num,reminder,reverse=0,contain;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    contain=num;
+    while(num!=0)
+     {
+        reminder=num%10;
+        reverse=reverse*10+reminder;
+        num/=10;
+     }
+    printf("Reverse of %d = %d \n",contain,reverse);
+    return 0; 
+ }
