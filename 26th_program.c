e5041c8cfdac4fb84bfb8785c8463ebc66ce6fff
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-15 20:24:23 +0530

    Program no 26 completed

--- 26th_program.c
+++ 26th_program.c
@@ -1,1 +1,17 @@
 // Write a program that shows the difference between scanf() and gets() for string.
+#include<stdio.h>
+int main()
+ {
+    char name[50];
+    printf("Enter your full name : ");
+    scanf("%s",name);       // This functon only reads single world string.
+    printf("%s \n",name);
+
+    printf("Enter your full name : ");
+    scanf("%[^\n]s",name);  //Alternate way to reads multi words string.
+    printf(" %s \n",name);
+
+    printf("Enter your full name : ");
+    gets(name);             // This is string function which can reads multi words string.
+    printf("%s \n",name);
+ } 
