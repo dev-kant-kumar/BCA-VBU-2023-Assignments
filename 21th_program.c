3378cb97e575ad2598c15a91966c7d9d7211877a
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-15 16:37:57 +0530

    Program no 21 completed

--- 21th_program.c
+++ 21th_program.c
@@ -1,1 +1,25 @@
 // Write a program to check whether entered number is palindrome or not.
+#include<stdio.h>
+int main()
+ {
+    int num,temp,reminder,reverse=0;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    temp=num;
+    while(temp!=0)
+     {
+        reminder=temp%10;
+        reverse=reverse*10+reminder;
+        temp/=10;
+     }
+    printf("Reverse: %d ",reverse); 
+    if(reverse==num)
+     {
+        printf("Entered number is a palindrome no \n");
+     }
+    else
+     {
+        printf("Entered number is not a plaindrome no \n");
+     
+   return 0;  } 
+ }
