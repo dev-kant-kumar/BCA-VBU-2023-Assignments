90da472f805fac2eab55c9d34cebb730b4f532a0
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-19 08:19:43 +0530

    Program no 18 completed

--- 18th_program.c
+++ 18th_program.c
@@ -1,1 +1,22 @@
 // Write a program to find first n fibonacci number.
+#include<stdio.h>
+int main()
+ {
+    int nterms,i;
+    printf("Enter the no of terms of series : ");
+    scanf("%d",&nterms);
+    int fibo_series[nterms];
+    fibo_series[0]=0;
+    fibo_series[1]=1;
+    for(i=2;i<nterms;i++)
+     {
+        fibo_series[i]=fibo_series[i-1]+fibo_series[i-2];
+     }
+    printf("Firs %d fibonacci numbers are below: \n",nterms);
+    for(i=0;i<nterms;i++)
+     {
+        printf("%d \t",fibo_series[i]);
+     } 
+    return 0; 
+
+ }
