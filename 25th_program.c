11e3ef309acda557ae240ad2cd93043bd30c7fd1
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-23 09:03:29 +0530

    Program no 25 completed

--- 25th_program.c
+++ 25th_program.c
@@ -1,1 +1,15 @@
-// Write a program that read a line of text containing a series of words using of getchar() and putchar() function.  
+// Write a program that read a line of text containing a series of words using of getchar() and putchar() function.
+#include<stdio.h>
+int main()
+ {
+    char ch;
+    printf("Enter your line of text : ");
+    while(ch!='\n')
+     {
+        ch=getchar();
+        putchar(ch);
+     }
+    printf("The above line is what you entered \n");
+    printf("\n");
+    return 0; 
+ }
