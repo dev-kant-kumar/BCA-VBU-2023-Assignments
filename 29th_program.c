6f3c4ff8c858a8fab67f8107769942c12db95ae4
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-25 11:52:05 +0530

    Program no 29 completed

--- 29th_program.c
+++ 29th_program.c
@@ -1,1 +1,26 @@
 // Write a program to find fibonacci series using recursion.
+#include<stdio.h>
+int fiboN(int n);
+int main()
+ {
+    int terms;
+    printf("Enter a number : ");
+    scanf("%d",&terms);
+    for(int i=0;i<terms;i++)
+     {
+        printf("%d \t",fiboN(i));
+     }
+    return 0;
+ }
+int fiboN(int n)
+ {
+    if(n<=1)
+     {
+        return n;
+     }
+    else
+     {
+        return fiboN(n-1)+fiboN(n-2);
+     } 
+
+ } 
