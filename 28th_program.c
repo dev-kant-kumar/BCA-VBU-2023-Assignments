cca5626b1630f312a663fcc2844e7bfd3493c90b
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-11-25 11:25:14 +0530

    Program no 28 completed

--- 28th_program.c
+++ 28th_program.c
@@ -1,1 +1,20 @@
 //Write a program to find factorial using recursion.
+#include<stdio.h>
+int fact(int num);
+int main()
+ {
+    int num;
+    printf("Enter a number to find their factorial : ");
+    scanf("%d",&num);
+    printf("Factorial of %d :%d \n",num,fact(num));
+ }
+int fact(int num)
+ {
+    if(num==1)
+     {
+        return 1;
+     }
+    int factm1=fact(num-1);
+    int factN=factm1*num;
+    return factN;
+ } 
