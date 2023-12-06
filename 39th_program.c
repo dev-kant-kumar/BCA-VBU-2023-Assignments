4753e27395337cb6896e36bae34801bae932b59d
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-12-06 21:09:51 +0530

    Program no 39 completed

--- 39th_program.c
+++ 39th_program.c
@@ -1,1 +1,16 @@
 // Wrire a program that shows the use of chain of pointer.
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+ {
+    int *ptr,**ptr1=&ptr;
+    ptr=(int *)malloc(sizeof(int));
+    printf("Enter a number :");
+    scanf("%d",ptr);
+    printf("Address of ptr :%p \n",(void *)ptr);
+    printf("Entered number :%d \n",*ptr);
+    printf("Address of ptr :%p \n",(void *)ptr1);
+    printf("Value at ptr1  :%d \n",**ptr1);
+    free(ptr);
+    return 0;
+ }
