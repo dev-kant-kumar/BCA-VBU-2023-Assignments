5c2c9c5d5cb308578f16e5a5f7a9a5cfbb4c16f0
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-12-06 17:22:16 +0530

    Program no 12 completed

--- 12th_program.c
+++ 12th_program.c
@@ -2,5 +2,29 @@
 #include<stdio.h>
 int main()
  {
-    
+    int num;
+    a:
+    printf("Enter a number :");
+    scanf("%d",&num);
+    if(num<0)
+     {
+        printf("Enter valid positive number \n");
+        goto a; // Backward jump to label 'a'
+     }
+    else if(num%2==0)
+    {
+        goto b; // Forward jump to label 'b'
+    }
+    else if(num%2!=0)
+    {
+        goto c; // Forward jump to label 'c'
+    }
+    b:
+    printf("You have entered a even no\n");
+    goto d;
+    c:
+    printf("You have entered an odd number \n");
+    d:
+    return 0;
+
  }
