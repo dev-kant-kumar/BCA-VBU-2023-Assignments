b6047c2f9d6e0459a92052e28883820c81ebf563
Dev Kant Kumar <dev.mytechhandle@gmail.com>
2023-12-06 19:14:01 +0530

    Program no 41 completed

--- 41th_program.c
+++ 41th_program.c
@@ -1,1 +1,22 @@
 // Write a program using pointer to determine the length of character string.
+#include<stdio.h>
+int main()
+ {
+    int i=0,length=0,space=0;
+    char name[100],*ptr;
+    printf("Enter any string(suggestion :name,line of text):");
+    fgets(name,100,stdin);
+    ptr=name;
+    while(ptr[i]!='\0')
+     {
+        if(ptr[i]==' ')
+        {
+            space++;
+        }
+        length++;
+        i++;
+     }
+    printf("Length of charactrs string with spaces is %d \n",length-1);
+    printf("Length of charactrs string without spaces is %d \n",length-space-1);
+    return 0;
+ }
